4a9c7b0d02e2bc793070cd6a6d2fdf549d4a8248
venky maguluri <venky maguluri>
2020-12-12 21:19:23 +0530

    first commit

---
+++ videos/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+
+class MovieAdmin(admin.ModelAdmin):
+
+    fields = ['release_year','title','length']    
+
+    search_fields = ['title','length']
+
+    list_filter = ['release_year', 'length',]
+
+    list_display = ['title', 'length', 'release_year',]
+
+    list_editable = ['length']
+
+admin.site.register(models.Movie, MovieAdmin)
+admin.site.register(models.Customer)
