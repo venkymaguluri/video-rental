229251ff5f891f9e385f017e4223338a7d81f3db
venky maguluri <venky maguluri>
2020-12-12 21:33:48 +0530

    added comment to admin page

--- videos/admin.py
+++ videos/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from . import models
 
 class MovieAdmin(admin.ModelAdmin):
-
+    #customizations formed for admin
     fields = ['release_year','title','length']    
 
     search_fields = ['title','length']
